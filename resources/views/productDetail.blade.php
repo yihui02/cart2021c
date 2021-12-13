156bea2ded816361cd8c1c0c19398857c6919176
yihui <D200230C@sc.edu.my>
2021-12-13 16:35:36 +0800

    13/12/2021 updata

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -1,3 +1,32 @@
+@extends('layout')
+@section('content')
+<div class="row">
+    @foreach($products as $product)
+    <div class="col-sm-2"></div>
+    <div class ="col-sm-10">
+        <div class="card-body">
+            <div class="row">
+                <form action="{{ route('add.to.cart')}}" method="Post">
+                    @CSRF
+                    <div class="col-md-3">
+                        <h5 class="card-title"{{$product->name}}></h5>
+                        <input type="hidden" name="productID" value="{{$produt-id}}">
+                        <img src="{{asset('images/')}}/{{$product->image}}" alt="" width="100%" class="img-fluid">
+                    </div>
+                <div class="col-md-9">
+                    <br><br>
+                    <p class="card-text">{{$product->description}}</p>
+                    <div class="card-heading">Quantity <input type="number" min="1" max="10" name="quantity"> Available: {{$product->quantity}}</div><br><br>
+                    <div class="card-heading">RM {{$product->price}}</div>
+                    <button class="btn btn-danger btn-xs" type="submit">Add to Cart</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
 
+@endsection
 
         
